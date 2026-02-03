12129441ca2e76bdeb874b320ecac4f539a3a548
KEV-AJ <porcher3842@gmail.com>
2026-02-03 20:45:41 +0100

    add body home

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,26 +1,27 @@
+import React from "react";
 import { Link} from "react-router-dom";
 //barre de navigation
 const Navbar =() => {
     return(
-<nav class="navbar navbar-expand-lg bg-dark">
-  <div class="container-fluid">
-    <Link class="navbar-brand text-light" to="/">John Doe</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg bg-dark">
+  <div className="container-fluid">
+    <Link className="navbar-brand text-light" to="/">John Doe</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <Link class="nav-link active text-light" aria-current="page" to="/">Home</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active text-light" aria-current="page" to="/">Home</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link active text-light" aria-current="page" to="/services">services</Link>
-        </li><li class="nav-item">
-          <Link class="nav-link active text-light" aria-current="page" to="/portfolio">portfolio</Link>
-        </li><li class="nav-item">
-          <Link class="nav-link active text-light" aria-current="page" to="/contact">contact</Link>
-        </li><li class="nav-item">
-          <Link class="nav-link active text-light" aria-current="page" to="/mentions">mentions légales</Link>
+        <li className="nav-item">
+          <Link className="nav-link active text-light" aria-current="page" to="/services">services</Link>
+        </li><li className="nav-item">
+          <Link className="nav-link active text-light" aria-current="page" to="/portfolio">portfolio</Link>
+        </li><li className="nav-item">
+          <Link className="nav-link active text-light" aria-current="page" to="/contact">contact</Link>
+        </li><li className="nav-item">
+          <Link className="nav-link active text-light" aria-current="page" to="/mentions">mentions légales</Link>
         </li>
       </ul>
     </div>
