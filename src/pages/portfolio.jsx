a41e8a4fa394822f63db0d9618b631753f80c0d3
KEV-AJ <porcher3842@gmail.com>
2026-02-07 12:07:01 +0100

    add style portfolio

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,85 +1,102 @@
 
 const Portfolio = () => {
     return (
-        <div className="container-fluid mt-5">
-             <img className="img-fluid mt-4" src="/image/banner.jpg" alt="bandeaux décoratifs couleur bleu " />
-            <h2>Portfolio</h2>
-            <p>Voici quelques-unes de mes réalisations</p>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card h-100">
-      <img src="/image/portfolio/fresh-food.jpg" class="card-img-top img-fluid" alt="panier-de-légumes"/>
-      <div class="card-body">
-        <h5 class="card-title">Fresh Food</h5>
-        <p class="card-text">Site de vente de produits frais en ligne</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Site réalisé avec PHP et MySQL</small>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="/image/portfolio/restaurant-japonais.jpg" class="card-img-top img-fluid" alt="suchi-restaurant-japonnais"/>
-      <div class="card-body">
-        <h5 class="card-title">Restaurant Akira</h5>
-        <p class="card-text">Site de vente de produits frais en ligne</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Site réalisé avec WorldPress</small>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="/image/portfolio/espace-bien-etre.jpg" class="card-img-top img-fluid" alt="espace-bien-etre"/>
-      <div class="card-body">
-        <h5 class="card-title">Espace bien-être</h5>
-        <p class="card-text">Site de vente de produits frais en ligne</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Site réalisé avec LARAVEL</small>
-      </div>
-    </div>
-  </div>
-   <div class="col">
-    <div class="card h-100">
-      <img src="/image/portfolio/seo.jpg" class="card-img-top img-fluid" alt="écran-seo"/>
-      <div class="card-body">
-        <h5 class="card-title">SEO</h5>
-        <p class="card-text">Amélioration du référencement d'un site e-commerce</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Utilisation des outils SEO</small>
-      </div>
-    </div>
-  </div>
-   <div class="col">
-    <div class="card h-100">
-      <img src="/image/portfolio/coder.jpg" class="card-img-top img-fluid" alt="screen-écran-du-code-de-developpement-site"/>
-      <div class="card-body">
-        <h5 class="card-title">Création d'une API</h5>
-        <p class="card-text">Création d'une API RESTFULL publique</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">PHP - SYMFONY</small>
-      </div>
-    </div>
-  </div>
-   <div class="col">
-    <div class="card h-100">
-      <img src="/image/portfolio/screens.jpg" class="card-img-top img-fluid" alt="espace-de-travail-bureau"/>
-      <div class="card-body">
-        <h5 class="card-title">Maquette d'un site web</h5>
-        <p class="card-text">Création du prototype d'un site</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Réalisé avec FIGMA</small>
-      </div>
-    </div>
-  </div>
-</div>
+      <div className="container-fluid p-0">
+             <img className="img-fluid mt-5 h-auto" src="/image/banner.jpg" alt="bandeaux décoratifs couleur bleu " />
+            <div className="w-100 my-5  text-center">
+              <h2>Portfolio</h2>
+              <p>Voici quelques-unes de mes réalisations</p> 
+            <div className="w-25 mx-auto border-bottom  border-3 border-primary">
+            </div>
+            </div>
+
+        <div className="row row-cols-1 row-cols-md-3 g-4 pb-5 container-md mx-auto">
+          
+          <div className="col-md-4">
+            <div className="card h-100">
+              <img src="/image/portfolio/fresh-food.jpg" className="card-img-top img-fluid" alt="panier-de-légumes"/>
+            <div className="card-body text-center">
+              <h5 className="card-title">Fresh Food</h5>
+              <p className="card-text">Site de vente de produits frais en ligne</p>
+              <button type="button" class="btn btn-primary">Voir le site</button>
+            </div>
+            <div className="card-footer text-center">
+              <small className="text-body-secondary">Site réalisé avec PHP et MySQL</small>
+            </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card h-100">
+              <img src="/image/portfolio/restaurant-japonais.jpg" className="card-img-top img-fluid" alt="suchi-restaurant-japonnais"/>
+            <div className="card-body text-center">
+              <h5 className="card-title">Restaurant Akira</h5>
+              <p className="card-text">Site de vente de produits frais en ligne</p>
+              <button type="button" class="btn btn-primary">Voir le site</button>
+            </div>
+            <div className="card-footer text-center">
+              <small className="text-body-secondary">Site réalisé avec WorldPress</small>
+            </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card h-100">
+              <img src="/image/portfolio/espace-bien-etre.jpg" className="card-img-top img-fluid" alt="espace-bien-etre"/>
+            <div className="card-body text-center">
+              <h5 className="card-title">Espace bien-être</h5>
+              <p className="card-text">Site de vente de produits frais en ligne</p>
+              <button type="button" class="btn btn-primary">Voir le site</button>
+            </div>
+            <div className="card-footer text-center">
+              <small className="text-body-secondary">Site réalisé avec LARAVEL</small>
+            </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card h-100">
+              <img src="/image/portfolio/seo.jpg" className="card-img-top img-fluid" alt="écran-seo"/>
+            <div className="card-body text-center">
+              <h5 className="card-title">SEO</h5>
+              <p className="card-text">Amélioration du référencement d'un site e-commerce</p>
+              <button type="button" class="btn btn-primary">Voir le site</button>
+            </div>
+            <div className="card-footer text-center">
+              <small className="text-body-secondary">Utilisation des outils SEO</small>
+            </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card h-100">
+              <img src="/image/portfolio/coder.jpg" className="card-img-top img-fluid" alt="screen-écran-du-code-de-developpement-site"/>
+            <div className="card-body text-center">
+              <h5 className="card-title">Création d'une API</h5>
+              <p className="card-text">Création d'une API RESTFULL publique</p>
+              <button type="button" class="btn btn-primary">Voir le site</button>
+            </div>
+            <div className="card-footer text-center">
+              <small className="text-body-secondary">PHP - SYMFONY</small>
+            </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card h-100">
+              <img src="/image/portfolio/screens.jpg" className="card-img-top img-fluid" alt="espace-de-travail-bureau"/>
+            <div className="card-body text-center">
+              <h5 className="card-title">Maquette d'un site web</h5>
+              <p className="card-text">Création du prototype d'un site</p>
+              <button type="button" class="btn btn-primary">Voir le site</button>
+            </div>
+            <div className="card-footer text-center">
+              <small className="text-body-secondary">Réalisé avec FIGMA</small>
+            </div>
+            </div>
+          </div>
         </div>
+      </div>
     )
 }
 
