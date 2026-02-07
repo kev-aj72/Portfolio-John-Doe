19d4832f76139414519ee75e98cb742b08a23a56
KEV-AJ <porcher3842@gmail.com>
2026-02-07 17:58:07 +0100

    add style page contact

--- src/components/googlemapiframe.jsx
+++ src/components/googlemapiframe.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 function GoogleMapIframe() {
-    return(
-        <iframe src="http://bit.ly/4knfk7J." 
-                width="600" 
-                height="450"  
-                allowfullscreen="" 
+    return(  
+        <iframe
+                src="http://bit.ly/4knfk7J." 
+                height="500"
+                width="100%"
+                allowFullScreen="" 
                 loading="lazy" 
-                referrerpolicy="no-referrer-when-downgrade"
+                referrerPolicy="no-referrer-when-downgrade"
                 title="Google Map">
 
         </iframe>
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -3,7 +3,7 @@ const Portfolio = () => {
     return (
       <div className="container-fluid p-0">
              <img className="img-fluid mt-5 h-auto" src="/image/banner.jpg" alt="bandeaux décoratifs couleur bleu " />
-            <div className="w-100 my-5  text-center">
+            <div className="w-100 my-5  text-center col-md-4">
               <h2>Portfolio</h2>
               <p>Voici quelques-unes de mes réalisations</p> 
             <div className="w-25 mx-auto border-bottom  border-3 border-primary">
@@ -16,9 +16,9 @@ const Portfolio = () => {
             <div className="card h-100">
               <img src="/image/portfolio/fresh-food.jpg" className="card-img-top img-fluid" alt="panier-de-légumes"/>
             <div className="card-body text-center">
-              <h5 className="card-title">Fresh Food</h5>
+              <h3 className="card-title">Fresh Food</h3>
               <p className="card-text">Site de vente de produits frais en ligne</p>
-              <button type="button" class="btn btn-primary">Voir le site</button>
+              <button type="button" className="btn btn-primary">Voir le site</button>
             </div>
             <div className="card-footer text-center">
               <small className="text-body-secondary">Site réalisé avec PHP et MySQL</small>
@@ -30,9 +30,9 @@ const Portfolio = () => {
             <div className="card h-100">
               <img src="/image/portfolio/restaurant-japonais.jpg" className="card-img-top img-fluid" alt="suchi-restaurant-japonnais"/>
             <div className="card-body text-center">
-              <h5 className="card-title">Restaurant Akira</h5>
+              <h3 className="card-title">Restaurant Akira</h3>
               <p className="card-text">Site de vente de produits frais en ligne</p>
-              <button type="button" class="btn btn-primary">Voir le site</button>
+              <button type="button" className="btn btn-primary">Voir le site</button>
             </div>
             <div className="card-footer text-center">
               <small className="text-body-secondary">Site réalisé avec WorldPress</small>
@@ -44,9 +44,9 @@ const Portfolio = () => {
             <div className="card h-100">
               <img src="/image/portfolio/espace-bien-etre.jpg" className="card-img-top img-fluid" alt="espace-bien-etre"/>
             <div className="card-body text-center">
-              <h5 className="card-title">Espace bien-être</h5>
+              <h3 className="card-title">Espace bien-être</h3>
               <p className="card-text">Site de vente de produits frais en ligne</p>
-              <button type="button" class="btn btn-primary">Voir le site</button>
+              <button type="button" className="btn btn-primary">Voir le site</button>
             </div>
             <div className="card-footer text-center">
               <small className="text-body-secondary">Site réalisé avec LARAVEL</small>
@@ -58,9 +58,9 @@ const Portfolio = () => {
             <div className="card h-100">
               <img src="/image/portfolio/seo.jpg" className="card-img-top img-fluid" alt="écran-seo"/>
             <div className="card-body text-center">
-              <h5 className="card-title">SEO</h5>
+              <h3 className="card-title">SEO</h3>
               <p className="card-text">Amélioration du référencement d'un site e-commerce</p>
-              <button type="button" class="btn btn-primary">Voir le site</button>
+              <button type="button" className="btn btn-primary">Voir le site</button>
             </div>
             <div className="card-footer text-center">
               <small className="text-body-secondary">Utilisation des outils SEO</small>
@@ -72,9 +72,9 @@ const Portfolio = () => {
             <div className="card h-100">
               <img src="/image/portfolio/coder.jpg" className="card-img-top img-fluid" alt="screen-écran-du-code-de-developpement-site"/>
             <div className="card-body text-center">
-              <h5 className="card-title">Création d'une API</h5>
+              <h3 className="card-title">Création d'une API</h3>
               <p className="card-text">Création d'une API RESTFULL publique</p>
-              <button type="button" class="btn btn-primary">Voir le site</button>
+              <button type="button" className="btn btn-primary">Voir le site</button>
             </div>
             <div className="card-footer text-center">
               <small className="text-body-secondary">PHP - SYMFONY</small>
@@ -86,9 +86,9 @@ const Portfolio = () => {
             <div className="card h-100">
               <img src="/image/portfolio/screens.jpg" className="card-img-top img-fluid" alt="espace-de-travail-bureau"/>
             <div className="card-body text-center">
-              <h5 className="card-title">Maquette d'un site web</h5>
+              <h3 className="card-title">Maquette d'un site web</h3>
               <p className="card-text">Création du prototype d'un site</p>
-              <button type="button" class="btn btn-primary">Voir le site</button>
+              <button type="button" className="btn btn-primary">Voir le site</button>
             </div>
             <div className="card-footer text-center">
               <small className="text-body-secondary">Réalisé avec FIGMA</small>
