2f8ca931bcaa4c4157a2066e6144a7a09ef5fa87
KEV-AJ <porcher3842@gmail.com>
2026-02-07 19:40:23 +0100

    add style page mentions legales

--- src/components/address.jsx
+++ src/components/address.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Address = () => {
     return(
-          <address className="list-unstyled">
+            <address className="list-unstyled">
                             <li>John Doe</li>
                             <li><i className="bi bi-map pe-2"></i>40 rue Laure Diebold</li>
                             <li><i className="bi bi-geo-alt pe-2"></i>69009 Lyon,France</li>
                             <li><i className="bi bi-phone pe-2"></i>1020304050</li>
                             <li><i className="bi bi-envelope-at pe-2"></i>john.doe@gmail.com</li>
-                        </address>
+            </address>
     )
 }
 
--- src/pages/mentions legales.jsx
+++ src/pages/mentions legales.jsx
@@ -1,64 +1,65 @@
-
+import React from "react";
+import Address from "../components/address"
 const Mentions = () => {
     return (
-        <div className="container-fluid mt-5">
-            <h2>Mentions légales</h2>
-      <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h3 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Editeur du site
-      </button>
-    </h3>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <address>
-            <li>John Doe</li>
-            <li>40 rue Laure Diebold</li>
-            <li>69009 Lyon,France</li>
-            <li>1020304050</li>
-            <li>john.doe@gmail.com</li>
-        </address>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h3 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Hébergeur
-      </button>
-    </h3>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <h4>alwaysdata</h4>
-        <address>91 Rue du Faubourg Saint-Honoré, 75008 Paris</address>
-        <a href="https://www.alwaysdata.com"target="_blank"
-        rel="noopener noreferrer">www.alwaysdata.com</a>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h3 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Crédits
-      </button>
-    </h3>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <h4>Crédits</h4>
-        <p>Ce site a été réalisé par John Doe, étudiant au
-             <a href="https://www.centre-europeen-formation.fr/landing/multi-marque/?code=4464&utm_campaign=TOTAL-REF_PAYANT-Marque_CEF&utm_source=bing&utm_medium=cpc&msclkid=0e0dee9adfa11e95b934871e151705db"target="_blank"
-        rel="noopener noreferrer">Centre Européen de formation.</a></p>
-        <p>Les image utilisées sur ce site sont libres de droits et ont été obtenues sur le site
-          <a href="https://pixabay.com/fr/"target="_blank"
-        rel="noopener noreferrer">Pixabay</a> </p>
-        <p>La favicon de ce site a été fournie par
-        <a href="https://www.flaticon.com/"target="_blank"
-        rel="noopener noreferrer">John doe Icons erstellt von Freepik - Flaticon</a> </p> 
-      </div>
-    </div>
-  </div>
-</div>
- </div>  </div> </div>     
-       
+        <div className="container-fluid pt-5">
+          <div className="title-page pt-5 text-center">
+            <h3 className="w-25 mx-auto border-bottom  pb-4 border-5 border-primary">Mentions légales</h3>
+          </div>
+          
+          <div class="accordion container-md mx-auto py-5" id="accordionExample">
+            <div class="accordion-item">
+              <h4 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" 
+                aria-expanded="true" aria-controls="collapseOne">Editeur du site
+                </button>
+              </h4>
+            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <Address/>
+              </div>
+            </div>
+            </div>
+
+            <div class="accordion-item">
+              <h4 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" 
+                aria-expanded="false" aria-controls="collapseTwo">Hébergeur
+                </button>
+              </h4>
+            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <span>alwaysdata</span>
+                <address>91 Rue du Faubourg Saint-Honoré, 75008 Paris</address>
+                <i class="bi bi-globe pe-2"></i><a href="https://www.alwaysdata.com"target="_blank"
+                  rel="noopener noreferrer">www.alwaysdata.com</a>
+              </div>
+            </div>
+            </div>
+  
+            <div class="accordion-item">
+              <h4 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" 
+                aria-expanded="false" aria-controls="collapseThree">Crédits
+                </button>
+              </h4>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <span>Crédits</span>
+                  <p>Ce site a été réalisé par John Doe, étudiant au
+                    <a href="https://www.centre-europeen-formation.fr/landing/multi-marque/?code=4464&utm_campaign=TOTAL-REF_PAYANT-Marque_CEF&utm_source=bing&utm_medium=cpc&msclkid=0e0dee9adfa11e95b934871e151705db"target="_blank"
+                      rel="noopener noreferrer"> Centre Européen de formation.</a></p>
+                  <p>Les image utilisées sur ce site sont libres de droits et ont été obtenues sur le site
+                    <a href="https://pixabay.com/fr/"target="_blank"
+                      rel="noopener noreferrer"> Pixabay</a> </p>
+                  <p>La favicon de ce site a été fournie par
+                    <a href="https://www.flaticon.com/"target="_blank"
+                    rel="noopener noreferrer"> John doe Icons erstellt von Freepik - Flaticon</a> </p> 
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
     )
 }
 
